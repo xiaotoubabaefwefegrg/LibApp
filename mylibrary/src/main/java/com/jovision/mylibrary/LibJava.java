5dd97f9ec2bb878dd189345bfcf768b0f60831df
于云龙 <yuyunlong@jovision.com>
2021-04-02 10:29:59 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/jovision/mylibrary/LibJava.java
@@ -0,0 +1,15 @@
+package com.jovision.mylibrary;
+
+/**
+ * @ProjectName: LibApp
+ * @Package: com.jovision.mylibrary
+ * @ClassName: LibJava
+ * @Description: java类作用描述
+ * @CreateDate: 2021/4/2 10:08 AM
+ * @Version: 1.0
+ */
+public class LibJava {
+    public static String getName(){
+        return "good";
+    }
+}
